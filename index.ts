faea17fbcb32170c86f5c6c24a0e13fe522af308
Lior Kuperiu <kuperiu@gmail.com>
2021-10-18 13:00:00 +0200

    1

---
+++ index.ts
@@ -0,0 +1,14 @@
+import * as p from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+
+export interface ObjectStorageArgs {
+    readonly name?: p.Input<string>
+}
+
+export class ObjectStorage extends p.ComponentResource {
+    private args?: ObjectStorageArgs
+    constructor(name: string, args?: ObjectStorageArgs, opts?: p.ComponentResourceOptions) {
+      super('test:deployment:object', name, args, opts)
+      new aws.s3.Bucket(name)
+     }
+}
